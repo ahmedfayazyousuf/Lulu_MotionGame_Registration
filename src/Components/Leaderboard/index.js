29fb6d6b03252ffae0b8b93afe6d0fe5c5168645
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2024-01-21 23:58:00 +0400

    o
    o

--- src/Components/Leaderboard/index.js
+++ src/Components/Leaderboard/index.js
@@ -1,34 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import firebase from '../../firebase';
-import BackgroundLeaderboard from '../1_Assets/RegistrationBG.png';
-import DPWorldLogo from '../1_Assets/LuluLogo.png';
+import BackgroundLeaderboard from '../1_Assets/Images/LeaderboardBG.png';
+import LuluLogo from '../1_Assets/LuluLogo.png';
 
 const Leaderboard = () => {
   const [leaderboardData, setLeaderboardData] = useState([]);
-  // eslint-disable-next-line
-  const [usersData, setUsersData] = useState([]);
 
   const formatScore = (score) => {
-    const formattedScore = score.toFixed(2);
-    return formattedScore.replace(/^(\d)\./, '0$1.');
+    const formattedScore = Math.floor(score); // Use Math.floor to remove decimal places
+    return formattedScore < 10 ? `0${formattedScore}` : formattedScore.toString();
   };
 
   useEffect(() => {
 
-    const usersCollection = firebase.firestore().collection("Users");
-    // eslint-disable-next-line
-    const usersUnsubscribe = usersCollection.onSnapshot((querySnapshot) => {
-      const users = [];
-      querySnapshot.forEach((doc) => {
-        users.push({ id: doc.id, ...doc.data() });
-      });
-      setUsersData(users);
-      console.log("Users Collection:", users);
-    });
-
-
     const Leaderboard = firebase.firestore().collection("Leaderboard");
-    const query = Leaderboard.orderBy("Score", "asc").limit(10);
+    const query = Leaderboard.orderBy("Score", "desc").limit(10);
 
     const unsubscribe = query.onSnapshot((querySnapshot) => {
       const data = [];
@@ -45,139 +31,55 @@ const Leaderboard = () => {
 
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "100%",
-          height: "100vh",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: 'center',
-          backgroundColor: '#1E1450',
-          backgroundImage: `url(${BackgroundLeaderboard})`,
-          backgroundPosition: 'bottom',
-          backgroundRepeat: 'no-repeat',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginBottom: '5px',
-            paddingTop: '45px',
-          }}
-        >
-          <img style={{ width: '60%' }} src={DPWorldLogo} alt="NBALogo" />
-        </div>
+      <div style={{ display: "flex", flexDirection: "column", width: "100%", height: "100vh", justifyContent: "center", alignItems: "center", textAlign: 'center', backgroundColor: '#1E1450', backgroundImage: `url(${BackgroundLeaderboard})`, backgroundPosition: 'top', backgroundSize: ' 1080px 1920px', backgroundRepeat: 'repeat', }} >
+        
+        <img style={{ width: '300px', position: 'absolute', top: '30px', right: '30px'}} src={LuluLogo} alt="LuluLogo" />
 
-        <div
-          style={{
-            width: '100%',
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <div
-            style={{
-              backgroundColor: '#DA1E59',
-              margin: '50px',
-              padding: '0',
-              height: '120px',
-              width: '700px',
-              borderRadius: '100px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <h1 style={{ color: 'white', fontSize: '65px', marginTop: '30px' }}>HIGH SCORE</h1>
+        <div style={{ width: '100%', display: "flex", justifyContent: "center", alignItems: "center", }} >
+          <div style={{  backgroundImage: 'linear-gradient(#76C0F7, #0567B5)', margin: '50px', height: '120px', width: '700px', borderRadius: '15px', display: 'flex', justifyContent: 'center', alignItems: 'center'}} >
+            <h1 style={{ color: 'white', fontSize: '65px', marginTop: '30px' }}>LEADERBOARD</h1>
           </div>
         </div>
 
-        <div
-          style={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
+        <div style={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', }} >
           <div style={{ display: 'flex', flexDirection: 'column', width: '80%' }}>
             <div style={{ display: 'flex', width: '100%', marginBottom: '20px' }}>
-              <div
-                style={{
-                  flex: '40%',
-                  borderRadius: '100px',
-                  background: 'rgba(128,128,128,0)',
-                  display: 'flex',
-                  justifyContent: 'flex-start',
-                  paddingLeft: '20px',
-                }}
-              ></div>
-              <div
-                style={{
-                  flex: '80%',
-                  borderRadius: '100px',
-                  justifyContent: 'space-between',
-                  marginLeft: '-340px',
-                  display: 'flex',
-                  justifyItems: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                <div style={{ paddingLeft: '90px', fontSize: '20px', color: 'white' }}>
-                  <h1>NAME</h1>
-                </div>
+              <div style={{ flex: '40%', borderRadius: '15px', background: 'rgba(128,128,128,0)', display: 'flex', justifyContent: 'flex-start', paddingLeft: '20px', }} ></div>
+                <div style={{ flex: '80%', borderRadius: '15px', justifyContent: 'space-between', marginLeft: '-340px', display: 'flex', justifyItems: 'center', alignItems: 'center'}} >
 
-                <div style={{ paddingRight: '90px', fontSize: '20px', color: 'white' }}>
-                  <h1>SCORE</h1>
-                </div>
+                  <div style={{ paddingLeft: '90px', fontSize: '20px', color: '#003561' }}>
+                    <h1>NAME</h1>
+                  </div>
+
+                  <div style={{ paddingRight: '90px', fontSize: '20px', color: '#003561' }}>
+                    <h1>SCORE</h1>
+                  </div>
+
               </div>
             </div>
 
             {leaderboardData.map((user, index) => (
               <div style={{ display: 'flex', width: '100%', marginBottom: '20px' }}>
-                <div
-                  style={{
-                    flex: '40%',
-                    borderRadius: '100px',
-                    background: 'rgba(128,128,128,0.4)',
-                    display: 'flex',
-                    justifyContent: 'flex-start',
-                    paddingLeft: '20px',
-                  }}
-                >
+
+                <div style={{ flex: '40%', borderRadius: '15px', backgroundColor: '#0567B5', display: 'flex', justifyContent: 'flex-start', paddingLeft: '30px', }} >
                   <h1 style={{ color: 'white', fontSize: '40px' }}>
                     {index + 1 === 10 ? index + 1 : `0${index + 1}`}
                   </h1>
                 </div>
 
-                <div
-                  style={{
-                    flex: '80%',
-                    borderRadius: '100px',
-                    background: 'white',
-                    justifyContent: 'space-between',
-                    marginLeft: '-340px',
-                    display: 'flex',
-                    justifyItems: 'center',
-                    alignItems: 'center',
-                  }}
-                >
+                <div style={{ flex: '80%', borderRadius: '15px',  backgroundImage: 'linear-gradient(#1E91D5, #80c8f2)', justifyContent: 'space-between', marginLeft: '-340px', display: 'flex', justifyItems: 'center', alignItems: 'center', }} >
                   <div style={{ paddingLeft: '90px', fontSize: '20px', color: '#1E1450' }}>
-                    <h1>{user.Name}</h1>
+                    <h1>{user.Name.split(' ')[0]}</h1>
                   </div>
 
                   <div style={{ paddingRight: '90px', fontSize: '20px', color: '#1E1450' }}>
                     <h1>{formatScore(user.Score)}</h1>
                   </div>
                 </div>
+
               </div>
             ))}
+
           </div>
         </div>
       </div>
